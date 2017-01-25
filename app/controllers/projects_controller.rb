f3d0cbadf4748052dcae00f806b1a6c0743bdad1
paulcijo <pl.cijo@gmail.com>
2017-01-26 01:05:04 +0530

    redirects the page to homepage when people access invalid records'

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,7 @@
 class ProjectsController < ApplicationController
+
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
+	
 	def index
 		@projects = Project.all
 	end
@@ -49,6 +52,13 @@ class ProjectsController < ApplicationController
 	end
 
 	private
+
+	def set_project
+		@project = Project.find(params[:id])
+	rescue ActiveRecord::RecordNotFound
+		flash[:alert] = "The project you were looking for could not be found."
+		redirect_to projects_path
+	end
 
 	def project_params
 		params.require(:project).permit(:name, :description)
